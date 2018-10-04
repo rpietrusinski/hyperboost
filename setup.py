feaa7d4c0dbb9c0513223fad332f2b3da89b81cb
Robert Pietrusinski <pietrusinski.robert@gmail.com>
2018-10-04 19:42:29 +0200

    Changeg structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='xg_vis',
+    version='0.1',
+    packages=['xg_vis',],
+    license='Open-source',
+    long_description=open('README.md').read(),
+)
